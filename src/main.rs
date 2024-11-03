7255d5053ab7f9ee07849a855ecea6c0c6fc830d
Joe Bloxsome <joe@bloxso.me>
2024-11-03 18:12:46 +0000

    run forward function

--- src/main.rs
+++ src/main.rs
@@ -40,33 +40,38 @@ fn main() -> ! {
         arduino_hal::delay_us(20000 - pulse_width);
     }
 
-    // Calibration mode
-    loop {
+    fn run_forward<PIN0: OutputPin, PIN1: OutputPin, PIN2: OutputPin>(servo0: &mut PIN0, servo1: &mut PIN1, servo2: &mut PIN2) {
         // Move servo1 to the rightmost position (this is the servo which controls the ant's middle leg).
         // Moving to the rightmost position moves the middle leg down, which lifts the ant's body up
         // and allows the other legs to push it forward.
-        for _ in 0..10 {
-            set_servo_position(&mut servo1, SERVO1_MAX_PULSE);
+        for _ in 0..20 {
+            set_servo_position(servo1, SERVO1_MAX_PULSE);
         }
 
         // Move servo0 and servo2 to min (these are the front and back servos which control the ant's front and back legs). Moving to min moves the
         // left legs back and right legs forward.
-        for _ in 0..10 {
-            set_servo_position(&mut servo0, SERVO0_MIN_PULSE);
-            set_servo_position(&mut servo2, SERVO2_MIN_PULSE);
+        for _ in 0..20 {
+            set_servo_position(servo0, SERVO0_MIN_PULSE);
+            set_servo_position(servo2, SERVO2_MIN_PULSE);
         }
 
         // Move servo1 to the leftmost position (this is the servo which controls the ant's middle leg). Moving to the leftmost position moves the
         // middle leg up, which drops the ant's body down and allows the legs to push it forward.
-        for _ in 0..10 {
-            set_servo_position(&mut servo1, SERVO1_MIN_PULSE);
+        for _ in 0..20 {
+            set_servo_position(servo1, SERVO1_MIN_PULSE);
         }
 
         // Move servo0 and servo2 to max (these are the front and back servos which control the ant's front and back legs). Moving to max moves the
         // left legs forward and right legs back.
-        for _ in 0..10 {
-            set_servo_position(&mut servo0, SERVO0_MAX_PULSE);
-            set_servo_position(&mut servo2, SERVO2_MAX_PULSE);
+        for _ in 0..20 {
+            set_servo_position(servo0, SERVO0_MAX_PULSE);
+            set_servo_position(servo2, SERVO2_MAX_PULSE);
         }
+    }
+
+
+    // Calibration mode
+    loop {
+        run_forward(&mut servo0, &mut servo1, &mut servo2);
     }
 }
